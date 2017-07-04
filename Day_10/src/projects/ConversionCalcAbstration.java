dc14449e2e089dcbfdcca7bab8cb1584a4a7e837
mnimmz <nimmons92@gmail.com>
2017-07-04 18:08:14 -0400

    Day 10 homework

---
+++ Day_10/src/projects/ConversionCalcAbstration.java
@@ -0,0 +1,17 @@
+package projects;
+
+import java.util.Scanner;
+
+public abstract class ConversionCalcAbstration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+
+	public abstract void feet();
+	
+	public abstract void celcius();
+
+}
